c9e50afc371f9ee1b26374d41b78b9ef908a5bfe
wotlk-addon <73294893+wotlk-addon@users.noreply.github.com>
2021-01-22 20:09:14 +0100

    Update DBM-Core.lua

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -58,10 +58,10 @@ f:SetScript("OnUpdate", fCLFix)
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 5093 $"):sub(12, -3),
-	Version = "5.093",
-	DisplayVersion = "5.093 Frostmourne", -- the string that is shown as version
-	ReleaseRevision = 5093 -- the revision of the latest stable version that is available (for /dbm ver2)
+	Revision = ("$Revision: 50933 $"):sub(12, -3),
+	Version = "5.0933",
+	DisplayVersion = "5.0933 Frostmourne", -- the string that is shown as version
+	ReleaseRevision = 50933 -- the revision of the latest stable version that is available (for /dbm ver2)
 }
 
 DBM_SavedOptions = {}
